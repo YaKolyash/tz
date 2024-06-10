41a89a7d651800fb9fa21abdbd489cf314c2709c
Nikolay <kolyashadrin1@gmail.com>
2024-06-10 17:11:28 +0300

    add files

---
+++ script/script.js
@@ -0,0 +1,32 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+
+    grid: {
+      rows: 2,
+    },
+
+    slidesPerColumn: 4,
+    slidesPerView: 4,
+    spaceBetween: 24,
+    slidesPerGroup: 6,
+});
+
+
